abc3564baee0a7090b08f687f35351398df1714b
hsimao <e087754958@gmail.com>
2021-02-08 14:17:05 +0800

    add: 新增顯示物理作用範圍 debug 用

--- assets/scripts/Platforms.js
+++ assets/scripts/Platforms.js
@@ -16,7 +16,15 @@ cc.Class({
 
   // LIFE-CYCLE CALLBACKS:
 
-  // onLoad () {},
+  onLoad() {
+    // debug 顯示物理作用範圍線條、區塊
+    cc.director.getPhysicsManager().debugDrawFlags =
+      cc.PhysicsManager.DrawBits.e_aabbBit |
+      cc.PhysicsManager.DrawBits.e_pairBit |
+      cc.PhysicsManager.DrawBits.e_centerOfMassBit |
+      cc.PhysicsManager.DrawBits.e_jointBit |
+      cc.PhysicsManager.DrawBits.e_shapeBit;
+  },
 
   start() {
     this.platforms = [];
